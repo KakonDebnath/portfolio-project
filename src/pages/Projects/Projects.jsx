5cf2a9fa7e0a1441123f3a82c0f447657ca77a1e
Kakon Debnath <kakondebnath96@gmail.com>
2023-08-16 20:52:10 +0600

    add another project

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -24,6 +24,15 @@ function Projects() {
       repoLink_server: 'https://github.com/KakonDebnath/kabbo_toy_server',
     },
     {
+      title: 'Kabbo Toys',
+      description: ' The Toy Store is an exciting online destination for toy lovers. Users can register using their email and password or choose to login with their Google or GitHub accounts, benefiting from the secure authentication framework provided by Google Firebase. Once authenticated, users can explore the various features of the platform, including "Add A Toy," "My Toys," and "Toy Details." In the "Add A Toy" section, users can effortlessly create new toy listings, providing detailed information such as name, description, price, and images. The "My Toys" section allows users to manage their toy inventory, enabling them to update, delete, and monitor their listed toys. With the Toy Store\'s seamless authentication powered by Google Firebase, toy enthusiasts can enjoy a convenient and trustworthy toy shopping experience.',
+      technologies: ['React', 'Express.js', 'MongoDB', 'Firebase'],
+      screenshot: 'https://i.ibb.co/b6Y870W/Home-Kabbo-Toys.png',
+      LiveLink: 'https://kabbo-toys.web.app/',
+      repoLink_client: 'https://github.com/KakonDebnath/kabbo-toys-client',
+      repoLink_server: 'https://github.com/KakonDebnath/kabbo_toy_server',
+    },
+    {
       title: 'Chef Master',
       description: 'The Chef recipe website is a platform where users can register using their email and password or choose to login with their Google or GitHub accounts. The website leverages Google Firebase for authentication, ensuring a secure login process. Once logged in, users can explore the Chef recipe page, which features a wide range of culinary delights. The website implements strict authentication checks to ensure only authorized users can access the recipe page, guaranteeing privacy and security. With an intuitive user interface and extensive recipe collection, the website offers a seamless experience for cooking enthusiasts to discover and share their favorite recipes.',
       technologies: ['React', 'Express.js', 'Firebase', 'react-ratings', 'swiper js'],
